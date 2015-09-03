289cd6939ee98c0dbc3ffa57bc82e977195202f9
Andreas Palsner <andreas.palsner@tigu.hk.tlu.ee>
2015-09-03 12:38:38 +0300

    asukohatade vahe

--- algus/asukohaProov.java
+++ algus/asukohaProov.java
@@ -2,8 +2,9 @@ public class asukohaProov{
    public static void main(String[] arg){
       asukoht haapsalu=new asukoht(58.936864, 23.540753);
 	  asukoht tartu=new asukoht(58.371829, 26.729506);
-      System.out.println(haapsalu+" "+tartu);
-	  System.out.println(haapsalu.getLat());
+      //System.out.println(haapsalu+" "+tartu);
+	  //System.out.println(haapsalu.getLat());
+	  System.out.println(tartu.kaugus(haapsalu));
 	  asukoht[] teekond=new asukoht[3];
 	  teekond[0]=haapsalu;
 	  teekond[1]=new asukoht(58.887944, 25.568624);
