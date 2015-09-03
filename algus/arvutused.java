9e01bb8b837b8c238906ed305f1e28c3abe64e79
Andreas Palsner <andreas.palsner@tigu.hk.tlu.ee>
2015-09-03 11:20:04 +0300

    uus1

--- algus/arvutused.java
+++ algus/arvutused.java
@@ -1,19 +1,25 @@
-import java.io*.;
+import java.io.*;
 public class arvutused{
-	public static void main (String[] arg){
-			throws IOException{
-		double kotimass=2.5;
-		int kottidearv=3;
-		System.out.println("Kokku: "+kotimass*kottidearv);
-		System.out.println("Mitu kotti seekord?");
-		BufferedReader lugeja=new BufferedReader();
-			new inputStreamReader(system.in)
-		);
-		String rida=lugeja.readLine();
-		try{
-			kottidearv=Integer.parseInt(rida);
-			System.out.println("Kokku: "+kotimass*kottidearv);
-		} catch(Exception ex){
-			System.out.println("Probleem: "+ex.getMessage());
+	static double kogumass(int kogus, double yhemass){
+		return kogus*yhemass;
 	}
+   public static void main(String[] arg) throws IOException{
+      double kotimass=2.5;
+	  int kottidearv=3;
+	  System.out.println("Kokku: "+kotimass*kottidearv);
+	  System.out.println("Mitu kotti seekord?");
+	  BufferedReader lugeja=new BufferedReader(
+	    new InputStreamReader(System.in)
+	  );
+	  String rida=lugeja.readLine();
+	  try{
+	     kottidearv=Integer.parseInt(rida);
+ 	     System.out.println("Kokku: "+kogumass(kottidearv, kotimass));		 
+		 for(int kotinr=1; kotinr<=kottidearv; kotinr++){
+		    System.out.println(kotinr+": "+kotinr*kotimass);
+		 }
+	  } catch(Exception ex){
+	     System.out.println("Probleem: "+ex.getMessage());
+	  }
+   }
 }
