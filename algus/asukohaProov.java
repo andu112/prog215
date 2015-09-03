20bde137b157ec761a8704e4d1eaa2bfe7089269
Andreas Palsner <andreas.palsner@tigu.hk.tlu.ee>
2015-09-03 12:23:10 +0300

    asukohaMuutus1

--- algus/asukohaProov.java
+++ algus/asukohaProov.java
@@ -4,5 +4,12 @@ public class asukohaProov{
 	  asukoht tartu=new asukoht(58.371829, 26.729506);
       System.out.println(haapsalu+" "+tartu);
 	  System.out.println(haapsalu.getLat());
+	  asukoht[] teekond=new asukoht[3];
+	  teekond[0]=haapsalu;
+	  teekond[1]=new asukoht(58.887944, 25.568624);
+	  teekond[2]=tartu;
+	  for(int i=0; i<teekond.length; i++){
+		System.out.println(teekond[i]);
+	  }
    }
 }
