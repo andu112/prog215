475036e102bbd8d18135866f702ad6093f6876d5
Andreas Palsner <andreas.palsner@tigu.hk.tlu.ee>
2015-09-03 12:09:37 +0300

    getter

--- algus/asukohaProov.java
+++ algus/asukohaProov.java
@@ -2,7 +2,7 @@ public class asukohaProov{
    public static void main(String[] arg){
       asukoht haapsalu=new asukoht(58.936864, 23.540753);
 	  asukoht tartu=new asukoht(58.371829, 26.729506);
-	  System.out.println("Ajavahe: "+
-	    (tartu.lng-haapsalu.lng)/360*24*60+" minutit");
+      System.out.println(haapsalu+" "+tartu);
+	  System.out.println(haapsalu.getLat());
    }
 }
