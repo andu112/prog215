16557c201585883a14534fbb5f8b466970cdaeaf
Andreas Palsner <andreas.palsner@tigu.hk.tlu.ee>
2015-09-17 12:31:14 +0300

    Algus

--- testid/ArvutusteTestid.java
+++ testid/ArvutusteTestid.java
@@ -7,4 +7,9 @@ public class ArvutusteTestid{
 		assertEquals(0.0, Arvutused.pikkus(0, 0), 0.01);
 		assertEquals(5.0, Arvutused.pikkus(3,-4), 0.01);
 	}	
+	@Test
+	public void punktideKauguseTest(){
+		assertEquals(1.41, Arvutused.punktideVahelineKaugus(2, 1, 1, 2), 0.01);
+		assertEquals(5, Arvutused.punktideVahelineKaugus(1, 5, 4, 1), 0.01);
+	}
 }
