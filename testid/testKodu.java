1ad93ef46e7a22ac3480af15f54e443a1808a253
Andreas Palsner <andreas.palsner@tigu.hk.tlu.ee>
2015-09-24 00:30:21 +0300

    KoduTesti

---
+++ testid/testKodu.java
@@ -0,0 +1,8 @@
+public class Arvutused{
+    public static double kiirus(double d, double t){
+        return kiirus(d/t);
+    }   
+    public static double kytus(double d){
+        return kytusk(d*7/100);
+    }
+}
