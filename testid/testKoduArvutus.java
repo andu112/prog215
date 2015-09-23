2e35624cc10e18ef8e06a24c6a0f3345aa2615e5
Andreas Palsner <andreas.palsner@tigu.hk.tlu.ee>
2015-09-24 00:59:27 +0300

    KoduTesti3

--- testid/testKoduArvutus.java
+++ testid/testKoduArvutus.java
@@ -1,14 +1,26 @@
 import org.junit.*;
 import static org.junit.Assert.*;
-public class testKoduArvutus{
+public class ArvutusteTestid{
 	@Test
-    public void kiiruseTest(){
-		assertEquals(5.0, Testid.kiirusA(5, 1), 0.01);
-		assertEquals(255.0, Testid.kiirusA(85, 3), 0.01);
-	}	/*
+    public void pikkuseTest(){
+		assertEquals(5.0, Arvutused.pikkus(3, 4), 0.01);
+		assertEquals(0.0, Arvutused.pikkus(0, 0), 0.01);
+		assertEquals(5.0, Arvutused.pikkus(3,-4), 0.01);
+	}	
+	//Koostage testfunktsioon kauguse leidmiseks kahe koordinaadipaari vahel tasandil
+	//Loo tegelik funktsioon tulemuse arvutamiseks, kĆ¤ivita testi kaudu.
 	@Test
-	public void kytuseTest(){
-		assertEquals(14, testKodu.kytusk(200), 0.01);
-		assertEquals(4.9, testKodu.kytusk(70), 0.01);
-	}*/
+	public void punktideKauguseTest(){
+		assertEquals(1.41, Arvutused.punktideVahelineKaugus(2, 1, 1, 2), 0.01);
+		assertEquals(5, Arvutused.punktideVahelineKaugus(1, 5, 4, 1), 0.01);
+	}
 }
+/*
+jaagup@tigu:~/public_html/15/prog215/testid$ javac -cp .:junit-4.12.jar ArvutusteTestid.java
+jaagup@tigu:~/public_html/15/prog215/testid$ java -cp .:junit-4.12.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore ArvutusteTestid
+JUnit version 4.12
+.
+Time: 0.053
+
+OK (1 test)
+*/
